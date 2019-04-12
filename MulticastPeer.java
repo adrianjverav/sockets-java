0570cf66bdf0254d4c2d46d9b990eb34f56670c0
Adrian Vera <vera.adrianj@gmail.com>
2019-04-12 01:12:41 -0400

    adaptar el multicast para recibir mensajes de varios clientes

--- MulticastPeer.java
+++ MulticastPeer.java
@@ -5,28 +5,33 @@ public class MulticastPeer{
     
     public static void main(String args[]){ 
 		// args give message contents and destination multicast group (e.g. "228.5.6.7")
+		String hostname = "228.5.6.7";
+		String message = "Lorem ipsum dolor sit amet.";
 		MulticastSocket s = null;
 		try {
-			InetAddress group = InetAddress.getByName(args[1]);
+			InetAddress group = InetAddress.getByName(hostname);
 			s = new MulticastSocket(6789);
 			s.joinGroup(group);
- 			byte [] m = args[0].getBytes();
+ 			byte [] m = message.getBytes();
 			DatagramPacket messageOut = new DatagramPacket(m, m.length, group, 6789);
 			s.send(messageOut);	
 			byte[] buffer = new byte[1000];
- 			for (int i=0; i< 3;i++) {		// get messages from others in group
+ 			for (int i = 0;i < 3;i++) {
  				DatagramPacket messageIn = new DatagramPacket(buffer, buffer.length);
  				s.receive(messageIn);
  				System.out.println("Received:" + new String(messageIn.getData()));
   			}
 			s.leaveGroup(group);		
 		}
-		catch (SocketException e){System.out.println("Socket: " + e.getMessage());
+		catch (SocketException e) {
+			System.out.println("Socket: " + e.getMessage());
 		}
-		catch (IOException e){System.out.println("IO: " + e.getMessage());
+		catch (IOException e) {
+			System.out.println("IO: " + e.getMessage());
 		}
 		finally {
-			if(s != null) s.close();
+			if(s != null)
+				s.close();
 		}
 	}		      	
 	
