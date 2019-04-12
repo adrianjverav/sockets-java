8fc5b6c9438cc7c38b5f5ea87f75eb9d451f9677
Adrian Vera <vera.adrianj@gmail.com>
2019-04-11 20:10:41 -0400

    prueba de recepción de mensaje enviado variando los tamaños del buffer

---
+++ UDPClient.java
@@ -0,0 +1,34 @@
+import java.net.*;
+import java.io.*;
+
+public class UDPClient{
+
+    public static void main(String args[]){
+    	String message = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi inventore dicta et, veniam adipisci distinctio, nostrum voluptates illo saepe excepturi deserunt autem, odit consequatur velit optio iure! Minus accusamus rem enim libero non laudantium, in error, consequuntur, eaque veritatis commodi porro nesciunt quo dolore debitis ex explicabo laboriosam quisquam! Provident quaerat aspernatur dignissimos cum corporis sint. Sapiente, tempore! Natus qui, nobis nisi temporibus, hic voluptatem inventore doloremque magni modi asperiores. Iure enim fugiat illo, reiciendis delectus labore officia neque? Perspiciatis tempore sapiente iure praesentium totam harum rerum veniam accusamus aspernatur est sint similique, in a numquam eveniet neque accusantium distinctio nemo facere laudantium incidunt ducimus inventore doloremque aliquid. Laudantium blanditiis dolor tempore quidem earum est, a neque itaque recusandae optio.";
+    	String hostname = "localhost";
+		DatagramSocket aSocket = null;
+
+		try {
+			aSocket = new DatagramSocket();    
+			byte [] m = message.getBytes();
+			InetAddress aHost = InetAddress.getByName(hostname);
+			int serverPort = 6789;		                                                 
+			DatagramPacket request = new DatagramPacket(m, message.length(), aHost, serverPort);
+			aSocket.send(request);			                        
+			byte[] buffer = new byte[100];
+			DatagramPacket reply = new DatagramPacket(buffer, buffer.length);	
+			aSocket.receive(reply);
+			System.out.println("Reply: " + new String(reply.getData()));	
+		}
+		catch (SocketException e){
+			System.out.println("Socket: " + e.getMessage());
+		}
+		catch (IOException e){
+			System.out.println("IO: " + e.getMessage());
+		}
+		finally {
+			if(aSocket != null)
+				aSocket.close();
+		}
+	}		      	
+}
