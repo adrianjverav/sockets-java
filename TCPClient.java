47529fd8aee21780634542792682df8868fbfc4f
Adrian Vera <vera.adrianj@gmail.com>
2019-04-12 00:14:45 -0400

    código para ver los mensajes enviados desde el cliente en el servidor

--- TCPClient.java
+++ TCPClient.java
@@ -1,30 +1,43 @@
+import java.util.Scanner;
 import java.net.*;
 import java.io.*;
 
 public class TCPClient {
 
 	public static void main (String args[]) {
-		// arguments supply message and hostname
+		String hostname = "localhost";
 		Socket s = null;
-		try{
-			int serverPort = 7896;
-			s = new Socket(args[1], serverPort);    
-			DataInputStream in = new DataInputStream( s.getInputStream());
-			DataOutputStream out =new DataOutputStream( s.getOutputStream());
-			out.writeUTF(args[0]);					// UTF is a string encoding see Sn. 4.4
-			String data = in.readUTF();			// read a line of data from the stream
-			System.out.println("Received: "+ data) ; 
+		Scanner input;
+
+		for (int i = 1; i <= 5; i++) {
+			System.out.println("Ingresar el mensaje a enviar:");
+	    	input = new Scanner(System.in);
+	    	String message = input.nextLine();
+
+			try{
+				int serverPort = 7896;
+				s = new Socket(hostname, serverPort);    
+				DataInputStream in = new DataInputStream( s.getInputStream());
+				DataOutputStream out =new DataOutputStream( s.getOutputStream());
+				out.writeUTF(message);					// UTF is a string encoding see Sn. 4.4
+				String data = in.readUTF();			// read a line of data from the stream
+				System.out.println("Received: "+ data) ; 
+			}
+			catch (UnknownHostException e){System.out.println("Socket:"+e.getMessage());
+			}
+			catch (EOFException e){System.out.println("EOF:"+e.getMessage());
+			}
+			catch (IOException e){System.out.println("readline:"+e.getMessage());
+			}
+			finally {
+				if (s!=null) 
+					try {
+						s.close();
+					}
+					catch (IOException e) {
+						System.out.println("close:"+e.getMessage());
+					}
+			}
 		}
-		catch (UnknownHostException e){System.out.println("Socket:"+e.getMessage());
-		}
-		catch (EOFException e){System.out.println("EOF:"+e.getMessage());
-		}
-		catch (IOException e){System.out.println("readline:"+e.getMessage());
-		}
-		finally {
-			if (s!=null) 
-			try {
-				s.close();
-			}catch (IOException e){System.out.println("close:"+e.getMessage());}}
 	}
 }
--- UDPClient.java
+++ UDPClient.java
@@ -1,24 +1,29 @@
+import java.util.Scanner;
 import java.net.*;
 import java.io.*;
 
 public class UDPClient{
 
     public static void main(String args[]){
-    	String message = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi inventore dicta et, veniam adipisci distinctio, nostrum voluptates illo saepe excepturi deserunt autem, odit consequatur velit optio iure! Minus accusamus rem enim libero non laudantium, in error, consequuntur, eaque veritatis commodi porro nesciunt quo dolore debitis ex explicabo laboriosam quisquam! Provident quaerat aspernatur dignissimos cum corporis sint. Sapiente, tempore! Natus qui, nobis nisi temporibus, hic voluptatem inventore doloremque magni modi asperiores. Iure enim fugiat illo, reiciendis delectus labore officia neque? Perspiciatis tempore sapiente iure praesentium totam harum rerum veniam accusamus aspernatur est sint similique, in a numquam eveniet neque accusantium distinctio nemo facere laudantium incidunt ducimus inventore doloremque aliquid. Laudantium blanditiis dolor tempore quidem earum est, a neque itaque recusandae optio.";
+    	System.out.println("Ingresar el mensaje a enviar:");
+    	Scanner input = new Scanner(System.in);
+    	String message = input.nextLine();
+
     	String hostname = "localhost";
 		DatagramSocket aSocket = null;
 
 		try {
-			aSocket = new DatagramSocket();    
+			aSocket = new DatagramSocket();
+            aSocket.setSoTimeout(6000);
 			byte [] m = message.getBytes();
 			InetAddress aHost = InetAddress.getByName(hostname);
 			int serverPort = 6789;		                                                 
 			DatagramPacket request = new DatagramPacket(m, message.length(), aHost, serverPort);
-			aSocket.send(request);			                        
+			aSocket.send(request);
 			byte[] buffer = new byte[100];
-			DatagramPacket reply = new DatagramPacket(buffer, buffer.length);	
+			DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
 			aSocket.receive(reply);
-			System.out.println("Reply: " + new String(reply.getData()));	
+			System.out.println("Reply: " + new String(reply.getData()));
 		}
 		catch (SocketException e){
 			System.out.println("Socket: " + e.getMessage());
