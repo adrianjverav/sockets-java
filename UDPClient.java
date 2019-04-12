aa9c115c5e7a69b8cc04c8e7d4e8959511f98550
Adrian Vera <vera.adrianj@gmail.com>
2019-04-12 01:23:39 -0400

    modiicar el updclient para que sólo quede para udp

--- UDPClient.java
+++ UDPClient.java
@@ -1,20 +1,14 @@
-import java.util.Scanner;
 import java.net.*;
 import java.io.*;
 
 public class UDPClient{
 
     public static void main(String args[]){
-    	// String message = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi inventore dicta et, veniam adipisci distinctio, nostrum voluptates illo saepe excepturi deserunt autem, odit consequatur velit optio iure! Minus accusamus rem enim libero non laudantium, in error, consequuntur, eaque veritatis commodi porro nesciunt quo dolore debitis ex explicabo laboriosam quisquam! Provident quaerat aspernatur dignissimos cum corporis sint. Sapiente, tempore! Natus qui, nobis nisi temporibus, hic voluptatem inventore doloremque magni modi asperiores. Iure enim fugiat illo, reiciendis delectus labore officia neque? Perspiciatis tempore sapiente iure praesentium totam harum rerum veniam accusamus aspernatur est sint similique, in a numquam eveniet neque accusantium distinctio nemo facere laudantium incidunt ducimus inventore doloremque aliquid. Laudantium blanditiis dolor tempore quidem earum est, a neque itaque recusandae optio.";
-    	String hostname = "228.5.6.7";
+    	String message = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum esse fuga in, ex officia magnam, laboriosam soluta quibusdam sit tempora, ipsum tempore iste quo culpa sequi molestias alias repellendus! Aliquam sit cum magnam nam, voluptate necessitatibus ea laboriosam natus, molestias.";
+    	String hostname = "localhost";
 		DatagramSocket aSocket = null;
-		Scanner input;
 		
 		try {
-			System.out.println("Ingresar el mensaje a enviar:");
-	    	input = new Scanner(System.in);
-			String message = input.nextLine();
-
 			aSocket = new DatagramSocket();    
 			byte [] m = message.getBytes();
 			InetAddress aHost = InetAddress.getByName(hostname);
